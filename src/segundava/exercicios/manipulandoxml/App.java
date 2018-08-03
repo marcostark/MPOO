86e39b232734e95c7dcac128f5dda1ec8860779d
Marcos <Marcos@PC>
2018-08-03 14:03:01 -0300

    Estudos de XML

---
+++ src/segundava/exercicios/manipulandoxml/App.java
@@ -0,0 +1,14 @@
+package segundava.exercicios.manipulandoxml;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa("Marcos","12345684797");
+		Pessoa p2 = new Pessoa("Patricia","12345684797");
+		
+		
+		
+	}
+
+}
---
+++ src/segundava/exercicios/manipulandoxml/PessoaDAO.java
@@ -0,0 +1,22 @@
+package segundava.exercicios.manipulandoxml;
+
+import java.util.List;
+
+public class PessoaDAO {
+	
+	
+	
+	public boolean adicionarPessoa(Pessoa pessoa) {
+		
+		
+		return false;
+		
+	}
+	
+	public List<Pessoa> pessoas (){
+		
+		
+		return null;
+	}
+
+}
